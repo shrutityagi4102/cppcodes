b1c5ef861b8027b9fa5bba90a4eebfeab46c7669
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-06-08 23:45:56 +0530

    Area of a circle

---
+++ circle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    float r,area;
+    cout<<"Enter the Radius : \n";
+    cin>>r;
+    area = M_PI*(r*r);
+    cout<<"The Area is : "<<area;
+}
