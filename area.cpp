09a0b118d5e9a7378656835e2373917c7a75dc1e
Shruti Tyagi <71224019+shrutityagi4102@users.noreply.github.com>
2022-06-08 23:19:37 +0530

    Area of a triangle

---
+++ area.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    float b,h;
+    cout<<"Enter base and height";
+    cin>>b>>h;
+    float area;
+    area = (float)(0.5)*(b*h);
+    cout<<"The Area is :"<<area;
+    return 0;
+}
